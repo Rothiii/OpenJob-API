e2bf6c37e0c9664d8aa2ef0c9e7d166dddc25d30
Rothiii <drome.emord@gmail.com>
2026-07-29 23:14:04 +0800

    feat: create migration for documents table with necessary fields

---
+++ migrations/1785337892869_create-table-documents.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('documents', {
+    id: { type: 'uuid', primaryKey: true, default: pgm.func('gen_random_uuid()') },
+    user_id: { type: 'uuid', notNull: true, references: '"users"', onDelete: 'cascade' },
+    filename: { type: 'text', notNull: true },
+    original_filename: { type: 'text', notNull: true },
+    created_at: { type: 'timestamp', notNull: true, default: pgm.func('current_timestamp') },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('documents');
+};
